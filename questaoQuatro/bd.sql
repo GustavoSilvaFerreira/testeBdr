c298f17bc485a9b784e91cf7a07a6dbd3b66611d
gustavoferreira <guto7626@hotmail.com>
2019-05-19 04:19:37 +0000

    insert table user

--- questaoQuatro/bd.sql
+++ questaoQuatro/bd.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS user (
 	PRIMARY KEY (cd_user)
 );
 
+INSERT INTO `user` (name) VALUES ('Gustavo');
+
 CREATE TABLE IF NOT EXISTS tarefas (
 	cd_tarefa INT NOT NULL AUTO_INCREMENT,
 	nm_titulo VARCHAR(255) NOT NULL,
